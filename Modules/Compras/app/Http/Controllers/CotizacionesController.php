443ef8dc4f58fbfebc9f6316f023a1eed22f4b85
desarrollo01Cob <desarrollo_01@cobama.com.mx>
2025-01-07 18:19:11 -0600

    Fix: Actualizar archivos

--- Modules/Compras/app/Http/Controllers/CotizacionesController.php
+++ Modules/Compras/app/Http/Controllers/CotizacionesController.php
@@ -18,16 +18,16 @@ use Modules\Compras\Models\SolicitudesCompra;
 
 class CotizacionesController extends Controller
 {
-
-    public function getFile($id, $file) 
-    { 
-        $path = storage_path("app/cotizaciones/$id/$file"); 
-        if (!File::exists($path)) { 
-            abort(404); 
-        } 
-        $fileContent = File::get($path); 
-        $type = File::mimeType($path); 
-        return response($fileContent, 200)->header("Content-Type", $type); 
+    //  Función para recuperar archivos del servidor 
+    public function getFile($id, $file)
+    {
+        $path = storage_path("app/cotizaciones/$id/$file");
+        if (!File::exists($path)) {
+            abort(404);
+        }
+        $fileContent = File::get($path);
+        $type = File::mimeType($path);
+        return response($fileContent, 200)->header("Content-Type", $type);
     }
     /**
      * Display a listing of the resource.
@@ -48,10 +48,10 @@ class CotizacionesController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request) 
-{
-    $data = $request->all();
-    
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
         foreach ($data['precios'] as $detalleId => $proveedores) {
             foreach ($proveedores as $proveedorId => $precio) {
                 DetallesCotizacion::create([
@@ -61,30 +61,28 @@ class CotizacionesController extends Controller
                 ]);
             }
         }
-    
-    
 
-    if ($request->hasFile('files')) {
-        foreach ($request->file('files') as $cotizacionProveedorId => $file) {
-            $cotizacionProveedor = CotizacionesProveedores::find($cotizacionProveedorId);
+        if ($request->hasFile('files')) {
+            foreach ($request->file('files') as $cotizacionProveedorId => $file) {
+                $cotizacionProveedor = CotizacionesProveedores::find($cotizacionProveedorId);
 
-            if ($cotizacionProveedor) {
-                $folderPath = 'cotizaciones/' . $cotizacionProveedor->cotizaciones_id;
-                $fileName = $cotizacionProveedor->proveedores_id . '.' . $file->getClientOriginalExtension();
+                if ($cotizacionProveedor) {
+                    $folderPath = 'cotizaciones/' . $cotizacionProveedor->cotizaciones_id;
+                    $fileName = $cotizacionProveedor->proveedores_id . '.' . $file->getClientOriginalExtension();
 
-                $path = $file->storeAs($folderPath, $fileName);
+                    $path = $file->storeAs($folderPath, $fileName);
 
-                $cotizacionProveedor->update(['ruta' => $path]);
+                    $cotizacionProveedor->update(['ruta' => $path]);
+                }
             }
         }
-    }
 
-    return response()->json([
-        'status' => 'success',
-        'message' => 'Se ha guardado correctamente',
-        'data' => []
-    ]);
-}
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Se ha guardado correctamente',
+            'data' => []
+        ]);
+    }
 
 
     /**
@@ -94,34 +92,33 @@ class CotizacionesController extends Controller
     // Recupera lel contenido de la tabla cotizaciones proveedores a través de solicitud de compra
     // DetalleSolicitudCompraResource::collection((DetalleSolicitud::where('solicitudes_compra_id', $id)->get()));
     public function show($id)
-{
-    $cotizacion = Cotizaciones::where('solicitudes_compra_id', $id)->first();
-    if ($cotizacion) {
-        $proveedores = ((CotizacionesProveedores::where('cotizaciones_id', $cotizacion->id)->get()));
-        $data = [];
-        foreach ($proveedores as $proveedor) {
-            $proveedorId = $proveedor->id;
-            $detalles = DetallesCotizacion::where('cotizaciones_proveedores_proveedores_id', $proveedorId)->get();
-            $nombreProveedor = Proveedores::where('id', $proveedor->proveedores_id)->get();
-            $proveedor->proveedores_id = $nombreProveedor;
-            $proveedor['detalles'] = $detalles;
+    {
+        $cotizacion = Cotizaciones::where('solicitudes_compra_id', $id)->first();
+        if ($cotizacion) {
+            $proveedores = ((CotizacionesProveedores::where('cotizaciones_id', $cotizacion->id)->get()));
+            $data = [];
+            foreach ($proveedores as $proveedor) {
+                $proveedorId = $proveedor->id;
+                $detalles = DetallesCotizacion::where('cotizaciones_proveedores_proveedores_id', $proveedorId)->get();
+                $nombreProveedor = Proveedores::where('id', $proveedor->proveedores_id)->get();
+                $proveedor->proveedores_id = $nombreProveedor;
+                $proveedor['detalles'] = $detalles;
 
-            $data[] = $proveedor;
+                $data[] = $proveedor;
+            }
+            return response()->json([
+                'status' => 'success',
+                'data' => $data,
+                'dataCotizacion' => $cotizacion
+            ]);
+        } else {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Cotización no encontrada',
+                'data' => []
+            ]);
         }
-        return response()->json([
-            'status' => 'success',
-            'data' => $data,
-            'dataCotizacion' => $cotizacion
-        ]);
-
-    } else {
-        return response()->json([ 
-            'status' => 'error', 
-            'message' => 'Cotización no encontrada', 
-            'data' => [] 
-        ]);
     }
-}
 
 
 
@@ -146,15 +143,12 @@ class CotizacionesController extends Controller
         $data = $request->all();
         $idSc = $data['0'];
         CotizacionesProveedores::where('id', $id)->update(['seleccionado' => 1]);
-        SolicitudesCompra::where('id', $idSc)->update(['estatus' => 5]);
-
+        SolicitudesCompra::where('id', $idSc)->update(['estatus' => 3]);
         return response()->json([
             'status' => 'success',
             'message' => 'Se ha actualizado correctamente',
             'data' => ''
         ]);
-
-
     }
 
     /**
--- Modules/Compras/app/Http/Controllers/ProveedoresController.php
+++ Modules/Compras/app/Http/Controllers/ProveedoresController.php
@@ -11,9 +11,11 @@ use Illuminate\Support\Facades\Storage;
 use Modules\Compras\Models\ExpedientesProveedores;
 
 use Modules\Compras\Models\Proveedores;
+
 /**
  * Resources
  */
+
 use Modules\Compras\Transformers\ProveedoresResource;
 
 class ProveedoresController extends Controller
@@ -43,16 +45,16 @@ class ProveedoresController extends Controller
         //  $proveedor = proveedores::create($request->all());
         //  $expediente = ExpedientesProveedores::create($request->all(), $proveedor);
 
-         $idProveedor =  $this->storeProveedor($request);
+        $idProveedor =  $this->storeProveedor($request);
 
-         $this->storeExpedienteProveedor($request, $idProveedor);
-        
+        $this->storeExpedienteProveedor($request, $idProveedor);
+
         return response()->json([
             'status' => 'success',
-             'message' => 'Se ha guardado correctamente',
-             //'data' => new ProveedoresResource($proveedor),
-             'data' => []
-         ]);
+            'message' => 'Se ha guardado correctamente',
+            //'data' => new ProveedoresResource($proveedor),
+            'data' => []
+        ]);
     }
 
 
@@ -60,8 +62,8 @@ class ProveedoresController extends Controller
      * Show the specified resource.
      */
     public function show($id)
-    {   
-        $expediente = ExpedientesProveedores::where('proveedores_id', $id)->first(); 
+    {
+        $expediente = ExpedientesProveedores::where('proveedores_id', $id)->first();
         return response()->json($expediente);
     }
 
@@ -79,9 +81,9 @@ class ProveedoresController extends Controller
     public function update(Request $request, $id)
     {
 
-          $this->updateProveedor($request, $id); 
-          
-          $this->updateExpedienteProveedor($request, $id);
+        $this->updateProveedor($request, $id);
+
+        $this->updateExpedienteProveedor($request, $id);
 
         return response()->json([
             'status' => 'success',
@@ -106,114 +108,143 @@ class ProveedoresController extends Controller
 
 
 
-private function storeProveedor($data)
-{
-    $dataProveedor = new Proveedores();
-    $dataProveedor->nombre = $data["nombre"];
-    $dataProveedor->contacto = $data["contacto"];
-    $dataProveedor->telefono = $data["telefono"];
-    $dataProveedor->localidad = $data["localidad"];
-    $dataProveedor->condiciones = $data["condiciones"];
-    $dataProveedor->servicios = $data["servicios"];
-    $dataProveedor->correo = $data["correo"];
-    $dataProveedor->dias_credito = $data["dias_credito"];
-    $dataProveedor->horario_atencion = $data["horario_atencion"];
-    $dataProveedor->tiempo_entrega = $data["tiempo_entrega"];
-    $dataProveedor->save();
-    return $dataProveedor->id;
-}
+    private function storeProveedor($data)
+    {
+        $dataProveedor = new Proveedores();
+        $dataProveedor->nombre = $data["nombre"];
+        $dataProveedor->contacto = $data["contacto"];
+        $dataProveedor->telefono = $data["telefono"];
+        $dataProveedor->localidad = $data["localidad"];
+        $dataProveedor->condiciones = $data["condiciones"];
+        $dataProveedor->servicios = $data["servicios"];
+        $dataProveedor->correo = $data["correo"];
+        $dataProveedor->dias_credito = $data["dias_credito"];
+        $dataProveedor->horario_atencion = $data["horario_atencion"];
+        $dataProveedor->tiempo_entrega = $data["tiempo_entrega"];
+        $dataProveedor->save();
+        return $dataProveedor->id;
+    }
 
-private function storeExpedienteProveedor($data, $idProveedor)
-{
-    $expedienteSolicitud = new ExpedientesProveedores();
-    $carpetaProveedor = 'expedientes/'. $idProveedor;
+    private function storeExpedienteProveedor($data, $idProveedor)
+    {
+        $expedienteSolicitud = new ExpedientesProveedores();
+        $carpetaProveedor = 'expedientes/' . $idProveedor;
         Storage::makeDirectory($carpetaProveedor);
 
         if ($data->hasFile('constancia_fiscal')) {
-            $constancia_fiscal = "constancia_fiscal".".".$data->file('constancia_fiscal')->getClientOriginalExtension();
-         $expedienteSolicitud->constancia_fiscal = $data->file('constancia_fiscal')->storeAs($carpetaProveedor,$constancia_fiscal); 
-            } 
+            $constancia_fiscal = "constancia_fiscal" . "." . $data->file('constancia_fiscal')->getClientOriginalExtension();
+            $expedienteSolicitud->constancia_fiscal = $data->file('constancia_fiscal')->storeAs($carpetaProveedor, $constancia_fiscal);
+        }
         if ($data->hasFile('ine')) {
-            $nombreArchivo = "ine".".".$data->file('ine')->getClientOriginalExtension();
-             $expedienteSolicitud->ine = $data->file('ine')->storeAs($carpetaProveedor,$nombreArchivo); 
-            } 
+            $nombreArchivo = "ine" . "." . $data->file('ine')->getClientOriginalExtension();
+            $expedienteSolicitud->ine = $data->file('ine')->storeAs($carpetaProveedor, $nombreArchivo);
+        }
         if ($data->hasFile('comprobante_domicilio')) {
-            $nombreArchivo = "comprobante_domicilio".".".$data->file('comprobante_domicilio')->getClientOriginalExtension();
-             $expedienteSolicitud->comprobante_domicilio = $data->file('comprobante_domicilio')->storeAs($carpetaProveedor,$nombreArchivo);
-            } 
+            $nombreArchivo = "comprobante_domicilio" . "." . $data->file('comprobante_domicilio')->getClientOriginalExtension();
+            $expedienteSolicitud->comprobante_domicilio = $data->file('comprobante_domicilio')->storeAs($carpetaProveedor, $nombreArchivo);
+        }
         if ($data->hasFile('estado_cuenta')) {
-            $nombreArchivo = "estado_cuenta".".".$data->file('estado_cuenta')->getClientOriginalExtension();
-             $expedienteSolicitud->estado_cuenta = $data->file('estado_cuenta')->storeAs($carpetaProveedor,$nombreArchivo);
-            } 
-        if ($data->hasFile('acta_constitutiva')) { 
-            $nombreArchivo = "acta_constitutiva".".".$data->file('acta_constitutiva')->getClientOriginalExtension();
-            $expedienteSolicitud->acta_constitutiva = $data->file('acta_constitutiva')->storeAs($carpetaProveedor,$nombreArchivo);
-            } 
-            
-        if ($data->hasFile('poder_notarial')) { 
-            $nombreArchivo = "poder_notarial".".".$data->file('poder_notarial')->getClientOriginalExtension();
-            $expedienteSolicitud->poder_notarial = $data->file('poder_notarial')->storeAs($carpetaProveedor,$nombreArchivo);
+            $nombreArchivo = "estado_cuenta" . "." . $data->file('estado_cuenta')->getClientOriginalExtension();
+            $expedienteSolicitud->estado_cuenta = $data->file('estado_cuenta')->storeAs($carpetaProveedor, $nombreArchivo);
+        }
+        if ($data->hasFile('acta_constitutiva')) {
+            $nombreArchivo = "acta_constitutiva" . "." . $data->file('acta_constitutiva')->getClientOriginalExtension();
+            $expedienteSolicitud->acta_constitutiva = $data->file('acta_constitutiva')->storeAs($carpetaProveedor, $nombreArchivo);
         }
 
-    $expedienteSolicitud->proveedores_id = $idProveedor;
-    $expedienteSolicitud->save();
-}
+        if ($data->hasFile('poder_notarial')) {
+            $nombreArchivo = "poder_notarial" . "." . $data->file('poder_notarial')->getClientOriginalExtension();
+            $expedienteSolicitud->poder_notarial = $data->file('poder_notarial')->storeAs($carpetaProveedor, $nombreArchivo);
+        }
 
-private function updateProveedor($data, $id)
-{
-    $proveedor = Proveedores::find($id); 
-    $proveedor->nombre = $data->nombre; 
-    $proveedor->contacto = $data->contacto; 
-    $proveedor->telefono = $data->telefono; 
-    $proveedor->localidad = $data->localidad; 
-    $proveedor->condiciones = $data->condiciones; 
-    $proveedor->servicios = $data->servicios; 
-    $proveedor->correo = $data->correo; 
-    $proveedor->dias_credito = $data->dias_credito; 
-    $proveedor->horario_atencion = $data->horario_atencion; 
-    $proveedor->tiempo_entrega = $data->tiempo_entrega; 
+        $expedienteSolicitud->proveedores_id = $idProveedor;
+        $expedienteSolicitud->save();
+    }
 
-    $proveedor->save();
+    private function updateProveedor($data, $id)
+    {
+        $proveedor = Proveedores::find($id);
+        $proveedor->nombre = $data->nombre;
+        $proveedor->contacto = $data->contacto;
+        $proveedor->telefono = $data->telefono;
+        $proveedor->localidad = $data->localidad;
+        $proveedor->condiciones = $data->condiciones;
+        $proveedor->servicios = $data->servicios;
+        $proveedor->correo = $data->correo;
+        $proveedor->dias_credito = $data->dias_credito;
+        $proveedor->horario_atencion = $data->horario_atencion;
+        $proveedor->tiempo_entrega = $data->tiempo_entrega;
 
-}
-/*---------------------------------------------------------------------
+        $proveedor->save();
+    }
+    /*---------------------------------------------------------------------
 *
     *Primero busco al proveedor
+    *Elimino el anterior archivo
     *Almaceno los archivos 
     *Despues almaceno las rutas del expediente
     *Finalemnte mostramos la solicitud con los detalles
 *---------------------------------------------------------------------
 */
- private function updateExpedienteProveedor($data, $idProveedor)
- {
-    $expediente = ExpedientesProveedores::where('proveedores_id', $idProveedor)->first();
-    $carpetaProveedor = 'expedientes/' . $idProveedor; Storage::makeDirectory($carpetaProveedor); 
-    if ($data->hasFile('constancia_fiscal'))
-       { 
-        $constancia_fiscal = "constancia_fiscal." . $data->file('constancia_fiscal')->getClientOriginalExtension();
-        $expediente->constancia_fiscal = $data->file('constancia_fiscal')->storeAs($carpetaProveedor, $constancia_fiscal);
+    private function updateExpedienteProveedor($data, $idProveedor)
+    {
+        $hoy = date("jnY"); //Recuperar la fecha del dia de hoy para diferenciar el registro nuevo
+        $expediente = ExpedientesProveedores::where('proveedores_id', $idProveedor)->first();
+
+        $carpetaProveedor = 'expedientes/' . $idProveedor;
+
+        Storage::makeDirectory($carpetaProveedor);
+        if ($data->hasFile('constancia_fiscal')) {
+
+            $archivoEliminar = $expediente->constancia_fiscal; //Recuperarla anterior ruta del archivo al a eliminar
+            Storage::delete($archivoEliminar); //Borrar el antiguo archivo
+
+            $constancia_fiscal = "constancia_fiscal" . $hoy . "." . $data->file('constancia_fiscal')->getClientOriginalExtension(); //Asignar un nombre al archivo
+            //$constancia_fiscal = "constancia_fiscal.". $data->file('constancia_fiscal')->getClientOriginalExtension();
+            $expediente->constancia_fiscal = $data->file('constancia_fiscal')->storeAs($carpetaProveedor, $constancia_fiscal); //Actualiza la ruta y el archivo
         }
         if ($data->hasFile('ine')) {
-            $nombreArchivo = "ine." . $data->file('ine')->getClientOriginalExtension();
-            $expediente->ine = $data->file('ine')->storeAs($carpetaProveedor, $nombreArchivo);
+
+            $archivoEliminar = $expediente->ine;
+            Storage::delete($archivoEliminar);
+
+                $nombreArchivo = "ine". $hoy . "." . $data->file('ine')->getClientOriginalExtension();
+                $expediente->ine = $data->file('ine')->storeAs($carpetaProveedor, $nombreArchivo);
         }
         if ($data->hasFile('comprobante_domicilio')) {
-            $nombreArchivo = "comprobante_domicilio." . $data->file('comprobante_domicilio')->getClientOriginalExtension();
-            $expediente->comprobante_domicilio = $data->file('comprobante_domicilio')->storeAs($carpetaProveedor, $nombreArchivo);
+
+            $archivoEliminar = $expediente->comprobante_domicilio;
+            Storage::delete($archivoEliminar);
+
+                $nombreArchivo = "comprobante_domicilio" . $hoy . ".". $data->file('comprobante_domicilio')->getClientOriginalExtension();
+                $expediente->comprobante_domicilio = $data->file('comprobante_domicilio')->storeAs($carpetaProveedor, $nombreArchivo);
         }
         if ($data->hasFile('estado_cuenta')) {
-            $nombreArchivo = "estado_cuenta." . $data->file('estado_cuenta')->getClientOriginalExtension();
-            $expediente->estado_cuenta = $data->file('estado_cuenta')->storeAs($carpetaProveedor, $nombreArchivo);
+
+            $archivoEliminar = $expediente->estado_cuenta;
+            Storage::delete($archivoEliminar);
+
+                $nombreArchivo = "estado_cuenta" . $hoy . ".". $data->file('estado_cuenta')->getClientOriginalExtension();
+                $expediente->estado_cuenta = $data->file('estado_cuenta')->storeAs($carpetaProveedor, $nombreArchivo);
         }
         if ($data->hasFile('acta_constitutiva')) {
-            $nombreArchivo = "acta_constitutiva." . $data->file('acta_constitutiva')->getClientOriginalExtension();
-            $expediente->acta_constitutiva = $data->file('acta_constitutiva')->storeAs($carpetaProveedor, $nombreArchivo);
+
+            $archivoEliminar = $expediente->acta_constitutiva;
+            Storage::delete($archivoEliminar);
+
+                $nombreArchivo = "acta_constitutiva". $hoy . "." . $data->file('acta_constitutiva')->getClientOriginalExtension();
+                $expediente->acta_constitutiva = $data->file('acta_constitutiva')->storeAs($carpetaProveedor, $nombreArchivo);
         }
         if ($data->hasFile('poder_notarial')) {
-            $nombreArchivo = "poder_notarial." . $data->file('poder_notarial')->getClientOriginalExtension();
-            $expediente->poder_notarial = $data->file('poder_notarial')->storeAs($carpetaProveedor, $nombreArchivo);
+
+            $archivoEliminar = $expediente->poder_notarial;
+            Storage::delete($archivoEliminar);
+            
+                $nombreArchivo = "poder_notarial" . $hoy . ".". $data->file('poder_notarial')->getClientOriginalExtension();
+                $expediente->poder_notarial = $data->file('poder_notarial')->storeAs($carpetaProveedor, $nombreArchivo);
         }
+
         $expediente->save();
 
- }
+    }
 }
