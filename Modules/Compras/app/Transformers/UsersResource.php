60e6112bcb5c31ce4e57c5c5fd9f50a10b2da6cf
desarrollo01Cob <desarrollo_01@cobama.com.mx>
2025-09-19 15:53:01 -0600

    Fix: filtro empresas

--- Modules/Compras/app/Transformers/UsersResource.php
+++ Modules/Compras/app/Transformers/UsersResource.php
@@ -13,7 +13,8 @@ class UsersResource extends JsonResource
     public function toArray(Request $request): array
     {
         $isAgencia = $this->setIsAgencia($this->intercompania);
-
+        $multiselect = $this->multiselect($this->intercompania);
+        $empresas = $this->setEmpresas($this->intercompania);
         return[
             "id" => $this->id,
             "firstname" => $this->firstname,
@@ -24,7 +25,9 @@ class UsersResource extends JsonResource
             "direccion" => $this->direccion ?? "N/A",
             "intercompania" => $this->intercompania,
             "empresa" => $this->empresa ?? "N/A",
-            "isAgencia"=> $isAgencia
+            "isAgencia"=> $isAgencia,
+            "multiselect" => $multiselect,
+            "empresas" => $empresas
         ];
     }
 
@@ -36,4 +39,28 @@ class UsersResource extends JsonResource
         $interAgencias = array_flip([7102, 7075, 7074, 7072, 7071, 7064, 7063, 7062, 7061, 7051, 712, 710, 706]);
         return isset($interAgencias[$intercompania]); 
     }
+
+    private function multiselect($intercompania)
+    {
+        $interAgencias = array_flip([333, 131, 130, 251, 210, 111, 250, 133, 354]);
+        return isset($interAgencias[$intercompania]); 
+    }
+
+    private function setEmpresas($intercompania){
+        $empresas = [
+            131 =>  [130, 131],
+            130 =>  [130, 131],
+            251 =>  [250, 251],
+            210 =>  [210, 111],
+            111 =>  [210, 111],
+            250 =>  [250, 251],
+            133 =>  [354, 133],
+            354 =>  [354, 133],
+        ];
+
+        return $empresas[$intercompania] ??  null;
+    }
+
+
+
 }
