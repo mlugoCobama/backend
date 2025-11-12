f7c59af44038907d73bea59a03afff5403a71760
desarrollo01Cob <desarrollo_01@cobama.com.mx>
2025-11-12 11:40:49 -0600

    Feat: add user seguimiento solicitud

--- Modules/Compras/app/Transformers/SeguimientoResource.php
+++ Modules/Compras/app/Transformers/SeguimientoResource.php
@@ -5,9 +5,12 @@ namespace Modules\Compras\Transformers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use App\Enums\EstatusSolicitud;
+use Illuminate\Support\Facades\DB;
 
 class SeguimientoResource extends JsonResource
 {
+    private static $usuariosCache = [];
+
     /**
      * Transform the resource into an array.
      */
@@ -15,39 +18,39 @@ class SeguimientoResource extends JsonResource
     {
         $evento = $this->setLabelEvento($this->event);
         $datosCambio = $this->parseCambio($this->new_values);
-        $mensajeCambio = $this->generarMensaje($this->event, $this->new_values );
-
+        $mensajeCambio = $this->generarMensaje($this->event, $datosCambio, $this->user_id);
         return [
             'id' => $this->id,
             'idSolicitud' => $this->record_id,
             'evento' => $evento,
             'mensaje' => $mensajeCambio,
-            'fecha' =>  $this->created_at
+            'fecha' => $this->created_at
         ];
     }
 
-    private function setLabelEvento( $evento ){
-        $estados = [
-            "created" => "Inicio del proceso ",
-            "updated" => "Se actualizo",
-        ];
-
-        return $estados[$evento]; 
-    }
-
-    
-
-    private function generarMensaje($evento, $values)
+    private function setLabelEvento($evento)
     {
+
+        $estados = [
+            "created" => "Inicio del proceso",
+            "updated" => "Se actualizó",
+        ];
+
+        return $estados[$evento] ?? 'Evento desconocido';
+    }
+
+    private function generarMensaje($evento, $values, $userId)
+    {
+        $nombreUsuarios = $this->getNombreUsuario($userId);
+
         switch ($evento) {
             case 'created':
-                return 'Se creó tu solicitud con folio: ' . ($values['folio'] ?? 'SIN FOLIO');
+                return 'Se creó tu solicitud con folio: ' . ($values['folio'] ?? 'SIN FOLIO') . "\n" . ' por ' . $nombreUsuarios;
 
             case 'updated':
-                
                 if (
-                array_key_exists('auto_gg', $values) && $values['auto_gg'] === 0 ||
-                array_key_exists('auto_admin', $values) && $values['auto_admin'] === 0
+                    (array_key_exists('auto_gg', $values) && $values['auto_gg'] === 0) ||
+                    (array_key_exists('auto_admin', $values) && $values['auto_admin'] === 0)
                 ) {
                     return 'Se requiere autorización de gerencia administrativa y gerencia general';
                 }
@@ -60,14 +63,14 @@ class SeguimientoResource extends JsonResource
 
                 foreach ($autorizaciones as $key => $mensaje) {
                     if (!empty($values[$key])) {
-                        return $mensaje;
+                        return $mensaje . "\n" . ' por ' . $nombreUsuarios;
                     }
                 }
 
                 if (isset($values['estatus'])) {
                     $labels = EstatusSolicitud::labels();
                     $label = $labels[$values['estatus']] ?? 'DESCONOCIDO';
-                    return 'El estatus de tu solicitud cambió a: ' . $label;
+                    return 'El estatus de tu solicitud cambió a: ' . $label . "\n" . ' por ' . $nombreUsuarios;
                 }
 
                 break;
@@ -79,9 +82,24 @@ class SeguimientoResource extends JsonResource
     private function parseCambio($cambio)
     {
         if (is_array($cambio)) return $cambio ?? null;
-        $json = json_decode($cambio, true);
-        return $json;
+        return json_decode($cambio, true);
     }
 
+    private function getNombreUsuario($usuarioId)
+    {
+        if (isset(self::$usuariosCache[$usuarioId])) {
+            return self::$usuariosCache[$usuarioId];
+        }
 
+        $usuario = DB::connection('intranet')->select('SELECT firstname, realname FROM glpi_users WHERE id = ?', [$usuarioId]);
+
+        if (!empty($usuario)) {
+            $nombreCompleto = $usuario[0]->firstname . ' ' . $usuario[0]->realname;
+            self::$usuariosCache[$usuarioId] = $nombreCompleto;
+            return $nombreCompleto;
+        }
+
+        return 'Usuario desconocido';
+    }
 }
+
